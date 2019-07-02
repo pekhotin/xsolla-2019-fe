a9e48b8ca1a4d4232662f47724d8b46785f8abea
pekhotin <pekhotin@saject.ru>
2019-07-03 02:42:07 +0500

    first commit

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <v-app>
+    <v-toolbar app>
+      <v-toolbar-title class="headline text-uppercase">
+        <span class="font-weight-light">Xsolla summer school 2019 FE</span>
+      </v-toolbar-title>
+    </v-toolbar>
+
+    <v-content>
+      <Data/>
+    </v-content>
+  </v-app>
+</template>
+
+<script>
+import Data from './components/Data'
+
+export default {
+  name: 'App',
+  components: {
+    Data
+  },
+  data () {
+    return {
+      //
+    }
+  }
+}
+</script>
---
+++ src/components/Data.vue
@@ -0,0 +1,33 @@
+<template>
+  <v-container>
+    <v-layout
+      text-xs-center
+      wrap
+    >
+      <v-flex xs12>
+        Здесь будут данные
+      </v-flex>
+
+    </v-layout>
+  </v-container>
+</template>
+
+<script>
+  export default {
+    data: () => ({
+      info: require('../assets/data.json')
+    }),
+    created() {
+      this.init()
+    },
+    methods: {
+      init () {
+
+      }
+    }
+  }
+</script>
+
+<style>
+
+</style>
