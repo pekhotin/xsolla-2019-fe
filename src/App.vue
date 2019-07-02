4c7887f4324fe383ff6614ff44f0f2a95857a59c
pekhotin <pekhotin@saject.ru>
2019-07-03 02:56:50 +0500

    Добавил две кнопки-заглушки

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,19 @@
       <v-toolbar-title class="headline text-uppercase">
         <span class="font-weight-light">Xsolla summer school 2019 FE</span>
       </v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-btn
+              flat
+              target="_blank"
+      >
+        <span class="mr-2">Я кнопка</span>
+      </v-btn>
+      <v-btn
+              flat
+              target="_blank"
+      >
+        <span class="mr-2">Я кнопка</span>
+      </v-btn>
     </v-toolbar>
 
     <v-content>
